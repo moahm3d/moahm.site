5e648950d47a6df325e262d61db97f915e4b8d1f
moahm3d <160626150+moahm3d@users.noreply.github.com>
2025-10-30 19:28:11 +0000

    Fix home page issues

--- scriptsheet.js
+++ scriptsheet.js
@@ -15,18 +15,44 @@ const initialScreen = document.getElementById('initial-screen');
 const continueBtn = document.getElementById('continue-btn');
 const navbar = document.querySelector('nav');
 const homePage = document.getElementById('home');
+const pastInitialScreen = sessionStorage.getItem('pastInitialScreen');
 
-document.body.style.overflow = 'hidden';
-document.body.style.height = '100vh';
-document.body.style.position = 'fixed';
-document.body.style.width = '100%';
+if ( initialScreen && !pastInitialScreen) {
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100vh';
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+} else {
+
+    
+    document.body.classList.remove('overflow-hidden');
+    document.body.style.overflow = '';
+    document.body.style.height = '';
+    document.body.style.position = '';
+    document.body.style.width = '';
+    if (initialScreen) {
+        initialScreen.style.display = 'none';
+    }
+
+    if (navbar) {
+        navbar.classList.remove('invisible');
+    }
+
+    if (homePage) {
+        homePage.classList.remove('invisible')
+    }
+}
 
 document.querySelectorAll('.scroll-reveal, .scroll-reveal-left, .scroll-reveal-right, .scroll-reveal-scale').forEach(el => {
     observer.observe(el);
 });
 
+if (continueBtn && initialScreen) {
 continueBtn.addEventListener('click', () => { 
 
+    sessionStorage.setItem('pastInitialScreen', 'true');
+
+        
     initialScreen.classList.add('initialScreen-fade-out');
 
     setTimeout(() => { 
@@ -42,10 +68,10 @@ continueBtn.addEventListener('click', () => {
         navbar.classList.add('navbar-slide');
 
         if (homePage) {
-            homePage.classList.remove('invisible');
+            homePage.classList.remove('invisible')
             homePage.classList.add('initialScreen-fade-in');
         }
     }, 900);
 });
-
+}
 console.log('Test load complete')
